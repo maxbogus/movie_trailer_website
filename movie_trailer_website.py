df2840378dfc8d9be24c68b0eb9600eba2069c81
Max Boguslavskiy <max.bogus@gmail.com>
2016-07-19 01:03:19 +0300

    * fixed pep8 errors.

--- movie_trailer_website.py
+++ movie_trailer_website.py
@@ -3,38 +3,38 @@ import media
 
 saw = media.Movie(
     'Saw',
-    'Two strangers discover they are pawns in a deadly game perpetrated by a notorious serial killer.',
-    'http://ia.media-imdb.com/images/M/MV5BMjE4MDYzNDE1OV5BMl5BanBnXkFtZTcwNDY2OTYwNA@@._V1_SY1000_SX675_AL_.jpg',
+    'Two strangers are pawns in a deadly game perpetrated by a serial killer.',
+    'http://goo.gl/K3dLh9',
     'https://www.youtube.com/watch?v=S-1QgOMQ-ls')
 
 terminator_par2 = media.Movie(
     'Terminator 2',
-    'A cyborg must now protect her young son from a more advanced cyborg, made out of liquid metal.',
-    'http://t1.gstatic.com/images?q=tbn:ANd9GcS5J6Ay6y1UT7WAI4U7Zm2KDYITrvfOI3vmaCNdGhx_0jmWiI1d',
+    'A cyborg must now protect her young son from a more advanced cyborg.',
+    'http://goo.gl/OQlnuQ',
     'https://www.youtube.com/watch?v=HgV7-MJwUBw')
 
 unforgiven = media.Movie(
     'Unforgiven',
-    'Retired Old West gunslinger William Munny reluctantly takes on one last job',
-    'http://ia.media-imdb.com/images/M/MV5BMTkzNTc0NDc4OF5BMl5BanBnXkFtZTcwNTY1ODg3OA@@._V1_.jpg',
+    'Retired gunslinger William Munny reluctantly takes on one last job',
+    'http://goo.gl/UfsbRv',
     'https://www.youtube.com/watch?v=N-bNAXtCa_A')
 
 beverly_hills_cop = media.Movie(
     'Beverly Hills Cop',
     'Using rock music to learn',
-    'http://www.gstatic.com/tv/thumb/movieposters/8676/p8676_p_v8_aa.jpg',
+    'http://goo.gl/S7E5KJ',
     'https://www.youtube.com/watch?v=XQGi4eB3RZI')
 
 true_detective = media.Movie(
     'True Detective',
-    'The first season follows a pair of detectives, and their pursuit of a serial killer over a 17-year period.',
-    'http://www.gstatic.com/tv/thumb/tvbanners/11689212/p11689212_b_v8_ac.jpg',
+    'Detectives are in pursuit of a serial killer over a 17-year period.',
+    'http://goo.gl/B0eEtE',
     'https://www.youtube.com/watch?v=mXG1netn9_g')
 
 fargo = media.Movie(
     'Fargo',
     'Fargo is a black-comedy drama',
-    'http://www.gstatic.com/tv/thumb/tvbanners/12031927/p12031927_b_v8_aa.jpg',
+    'http://goo.gl/qv29qv',
     'https://www.youtube.com/watch?v=gKs8DzjPDMU')
 
 movies = [
